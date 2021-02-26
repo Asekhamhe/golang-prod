68ee4d236fca8a50d2621d9ada11cf978d30b3d7
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-02-26 11:16:55 +0100

    solved exercises

--- golang-pro-2/ch1/echo1/main.go
+++ golang-pro-2/ch1/echo1/main.go
@@ -22,6 +22,17 @@ func main() {
 	// fmt.Println(strings.Join(os.Args[1:], " "))
 
 	// third version
-	fmt.Println(os.Args[1:])
+	// fmt.Println(os.Args[1:])
+
+	// ex 1
+	// fmt.Println(os.Args[0:])
+
+	// ex 2
+	for i, v := range os.Args[0:] {
+		fmt.Println(i)
+		fmt.Println(v)
+	}
+
+	// ex 3
 
 }
