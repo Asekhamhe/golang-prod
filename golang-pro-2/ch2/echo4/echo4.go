f6bc2cd679986f36a0744f5abc1daf5bdf30e928
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-03-02 10:48:27 +0100

    cli using flag

---
+++ golang-pro-2/ch2/echo4/echo4.go
@@ -0,0 +1,23 @@
+// Echo4 prints its command-line arguments.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+
+// Note: if the returned type is a pointer type, therefore the value is an address
+var sep = flag.String("s", " ", "seperator")
+
+func main() {
+	flag.Parse()
+	// *sep ===> access the value of an address
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
