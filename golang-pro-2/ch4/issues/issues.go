501330ab817a278db80e619ba3d3ef4f07d7894f
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-03-11 11:43:45 +0100

    report github issues less than a year old

--- golang-pro-2/ch4/issues/issues.go
+++ golang-pro-2/ch4/issues/issues.go
@@ -10,7 +10,8 @@ import (
 )
 
 func main() {
-	month, _ := time.Parse("02-01-2006", time.Now().AddDate(0, 0, -50).Format("02-01-2006"))
+	month, _ := time.Parse("02-01-2006", time.Now().AddDate(0, 0, -30).Format("02-01-2006"))
+	year, _ := time.Parse("02-01-2006", time.Now().AddDate(-1, 0, 0).Format("02-01-2006"))
 	res, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
@@ -18,6 +19,12 @@ func main() {
 	for _, v := range res.Items {
 		// report the results in age categories of less than a month
 		if d, _ := time.Parse("02-01-2006", v.CreatedAt.Format("02-01-2006")); d.After(month) {
+			fmt.Printf("%s #%-5d %9.9s %.55s\n", v.CreatedAt.Format("02-01-2006"), v.Number, v.User.Login, v.Title)
+		}
+
+		// report the result in age categories of less than a year
+
+		if y, _ := time.Parse("02-01-2006", v.CreatedAt.Format("02-01-2006")); y.After(year) {
 			fmt.Printf("%s #%-5d %9.9s %.55s\n", v.CreatedAt.Format("02-01-2006"), v.Number, v.User.Login, v.Title)
 		}
 
