16133395e9c1d3c4ce86f3bbe7f4b5a2b7390d61
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-03-16 15:49:10 +0100

    receiver method

--- golang-pro-2/ch6/geometry/geometry.go
+++ golang-pro-2/ch6/geometry/geometry.go
@@ -1,3 +1,5 @@
+// https://play.golang.org/p/xfHwQ-9CVxx
+// Difference between pointer receiver method and normal receiver method
 package main
 
 import (
@@ -10,6 +12,10 @@ type Point struct {
 }
 
 type Path []Point
+
+type Man struct {
+	age int
+}
 
 func Distance(p, q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
@@ -24,15 +30,42 @@ func (path Path) Distance() float64 {
 	sum := 0.0
 	for i := range path {
 		if i > 0 {
+			// fmt.Printf("%T", path[i-1])
 			sum += path[i-1].Distance(path[i])
 		}
 	}
 	return sum
 }
 
+// Method with a pointer receiver
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
+
+func (m Man) Display() {
+	m.age += 105
+	fmt.Println(m.age)
+}
+
+func DisplayPtr(m Man) {
+	fmt.Println(m.age)
+}
+
 func main() {
+
+	lawPtr := Man{10}
+
+	lawPtr.Display()
+	DisplayPtr(lawPtr)
+
 	p := Point{1, 2}
 	q := Point{4, 6}
+
+	r := Point{1, 2}
+	r.ScaleBy(2)
+
+	fmt.Println(r)
 	perim := Path{
 		{1, 1},
 		{5, 1},
