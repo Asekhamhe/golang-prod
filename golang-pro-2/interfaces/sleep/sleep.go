2b6a9ebb3c7e2dc913a0dc2b78358beacbbc8d41
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-03-17 13:03:48 +0100

    http handler interface

--- golang-pro-2/interfaces/sleep/sleep.go
+++ golang-pro-2/interfaces/sleep/sleep.go
@@ -10,7 +10,6 @@ var period = flag.Duration("period", 1*time.Second, "sleep period")
 
 func main() {
 	flag.Parse()
-	fmt.Printf("%T", period)
 	fmt.Printf("Sleeping for %v...", *period)
 	time.Sleep(*period)
 	fmt.Println()
